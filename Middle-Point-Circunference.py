cb5e76818689d0076e99014f938b9d38e533dfba
Sylviane Vitor <sylvianevitor@gmail.com>
2018-04-02 23:14:14 -0300

    Middle-Point corrigido

--- Middle-Point-Circunference.py
+++ Middle-Point-Circunference.py
@@ -3,42 +3,52 @@ import pygame
 pygame.init()
 
 #Set display configuration
-disp = pygame.display.set_mode((700,400))  #resolution
-disp.fill((1,1,1))						   #background color 
+disp = pygame.display.set_mode((400,400))  #resolution
+disp.fill((1,1,1))						   #background color
 pygame.display.flip() 					   #update display
 
 #total of pixels
 n_pixels = 0
 
 #Symmetry
-def CirclePoints(x, y, color): 
-	gfxdraw.pixel(disp, x,  y, color)
-	gfxdraw.pixel(disp, x, -y, color)
-	gfxdraw.pixel(disp, -x,  y, color)
-	gfxdraw.pixel(disp, -x, -y, color)
-	gfxdraw.pixel(disp,  y,  x, color)
-	gfxdraw.pixel(disp, y, -x, color)
-	gfxdraw.pixel(disp, -y,  x, color)
-	gfxdraw.pixel(disp, -y, -x, color)
+def CirclePoints(x, y, color):
+	global n_pixels
+	offset = 200 #used to plot in the center
+	gfxdraw.pixel(disp, x + offset,  y + offset, color)
+	gfxdraw.pixel(disp, x + offset, -y + offset, color)
+	gfxdraw.pixel(disp, -x + offset,  y + offset, color)
+	gfxdraw.pixel(disp, -x + offset, -y + offset, color)
+	gfxdraw.pixel(disp,  y + offset,  x + offset, color)
+	gfxdraw.pixel(disp, y + offset, -x + offset, color)
+	gfxdraw.pixel(disp, -y + offset,  x + offset, color)
+	gfxdraw.pixel(disp, -y + offset, -x + offset, color)
 	n_pixels = n_pixels + 8
 	pygame.display.flip() 					#update display
+	print(n_pixels)
 
 #Middle Point Circunference
-def MidPointCirc(r, color): 
+def MidPointCirc(r, color):
 	x = 0
 	y = r
-	d = 1 - r
+	d = 5 / 4 - r
 
-	CirclePoints(x, y, color);
+	CirclePoints(x, y, color)
 	while (y > x):
 		if (d < 0):
-			#Select E 
+			# Select E
 			d = d + 2 * x + 3
 			x = x + 1
 		else:
-			#Select  SE 
+			#Select SE
 			d = d + 2 * (x - y) + 5
 			x = x + 1
-		CirclePoints(x, y, color)
+			y = y - 1
+		CirclePoints(x,y,color)
 
 pygame.display.flip()				        #update display
+
+MidPointCirc(10,(255,255,255))
+
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: pygame.display.quit()   #quit display window
